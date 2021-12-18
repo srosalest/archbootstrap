17ebd37a7c2811aca77f7742ccb490547deee214
atejeda <atejeda@fastarch.localdomain>
2021-12-17 23:40:03 -0300

    First commit.

---
+++ 00.step.sh
@@ -0,0 +1,64 @@
+#!/usr/bin/env bash
+
+BASEDIR=$(cd -- "$(dirname -- "${BASH_SOURCE[0]}")" &> /dev/null && pwd)
+
+ENVFILE=$BASEDIR/environment
+source $ENVFILE
+cat $ENVFILE
+
+echo ""
+
+timedatectl set-ntp true
+
+pacman -Syy
+
+lsblk
+
+sgdisk --zap-all $ARCH_DEVICE
+
+sgdisk -n 1:0:+256M -t 1:ef00 -c 1:partition1 $ARCH_DEVICE
+sgdisk -i 1 $ARCH_DEVICE
+
+sgdisk -n 2:0:0 -t 2:8e00 -c 2:partition2 $ARCH_DEVICE
+sgdisk -i 2 $ARCH_DEVICE
+
+lsblk
+
+sync && sleep 5
+
+ARCH_DEVICE1=$(readlink -f /dev/disk/by-partlabel/partition1 2>&1)
+ARCH_DEVICE2=$(readlink -f /dev/disk/by-partlabel/partition2 2>&1)
+
+rm -rf $BASEDIR/environment.partitions
+echo "ARCH_DEVICE1=$ARCH_DEVICE1" >> $BASEDIR/environment.partitions
+echo "ARCH_DEVICE2=$ARCH_DEVICE2" >> $BASEDIR/environment.partitions
+cat $BASEDIR/environment.partitions
+
+cryptsetup luksFormat $ARCH_DEVICE2
+
+cryptsetup open $ARCH_DEVICE2 $ARCH_PVNAME
+
+pvcreate /dev/mapper/$ARCH_PVNAME
+vgcreate $ARCH_VGNAME /dev/mapper/$ARCH_PVNAME
+lvcreate -l 100%FREE $ARCH_VGNAME -n $ARCH_LVNAME
+
+lsblk
+
+mkfs.fat -F32 $ARCH_DEVICE1
+mkfs.ext4 /dev/$ARCH_VGNAME/$ARCH_LVNAME
+
+mount /dev/$ARCH_VGNAME/$ARCH_LVNAME /mnt
+mkdir -p /mnt/boot
+mount $ARCH_DEVICE1 /mnt/boot
+
+genfstab -U /mnt >> /mnt/etc/fstab
+
+pacstrap /mnt \
+         base base-devel linux linux-firmware lvm2 dialog git vim \
+         iwd dhclient netctl \
+         xorg dmenu picom nitrogen maim xscreensaver \
+         rxvt-unicode rxvt-unicode-terminfo mesa-libgl xterm xorg-xinit xterm
+
+
+rsync -avP $BASEDIR /mnt/bootstrap
+         
---
+++ 01.step.sh
@@ -0,0 +1,295 @@
+#!/usr/bin/env bash
+
+BASEDIR=$(cd -- "$(dirname -- "${BASH_SOURCE[0]}")" &> /dev/null && pwd)
+
+ENVFILE=$BASEDIR/environment
+source $ENVFILE
+cat $ENVFILE
+
+PARFILE=$BASEDIR/environment.partitions
+source $PARFILE
+cat $PARFILE
+
+echo ""
+
+ln -sf /usr/share/zoneinfo/America/Santiago /etc/localtime
+hwclock --systohc
+
+echo pacman -Syy linux linux-lts linux-headers linux-lts-headers iwd dhclient \
+       vim emacs base-devel openssh dialog lvm2 grub efibootmgr dosfstools \
+       os-prober mtools emacs git reflector cups xdg-utils xdg-user-dirs
+
+# locale
+
+echo "en_US.UTF-8 UTF-8" >> /etc/locale.gen
+echo "LANG=en_US.UTF-8" >> /etc/locale.conf
+locale-gen
+
+# hostname
+
+echo "$ARCH_HOSTNAME" > /etc/hostname
+
+cat <<EOF > /etc/hosts
+127.0.0.1 localhost
+::1 localhost
+127.0.0.1 $ARCH_HOSTNAME.localdomain $ARCH_HOSTNAME
+EOF
+
+# root password
+
+echo "root password"
+passwd
+
+# user
+
+echo "user password"
+useradd -m -g users -G wheel $ARCH_USER
+passwd $ARCH_USER
+
+# mkinitcpio.conf
+
+cat <<EOF > /etc/mkinitcpio.conf
+# vim:set ft=sh
+# MODULES
+# The following modules are loaded before any boot hooks are
+# run.  Advanced users may wish to specify all system modules
+# in this array.  For instance:
+#     MODULES=(piix ide_disk reiserfs)
+MODULES=()
+
+# BINARIES
+# This setting includes any additional binaries a given user may
+# wish into the CPIO image.  This is run last, so it may be used to
+# override the actual binaries included by a given hook
+# BINARIES are dependency parsed, so you may safely ignore libraries
+BINARIES=()
+
+# FILES
+# This setting is similar to BINARIES above, however, files are added
+# as-is and are not parsed in any way.  This is useful for config files.
+FILES=()
+
+# HOOKS
+# This is the most important setting in this file.  The HOOKS control the
+# modules and scripts added to the image, and what happens at boot time.
+# Order is important, and it is recommended that you do not change the
+# order in which HOOKS are added.  Run 'mkinitcpio -H <hook name>' for
+# help on a given hook.
+# 'base' is _required_ unless you know precisely what you are doing.
+# 'udev' is _required_ in order to automatically load modules
+# 'filesystems' is _required_ unless you specify your fs modules in MODULES
+# Examples:
+##   This setup specifies all modules in the MODULES setting above.
+##   No raid, lvm2, or encrypted root is needed.
+#    HOOKS=(base)
+#
+##   This setup will autodetect all modules for your system and should
+##   work as a sane default
+#    HOOKS=(base udev autodetect block filesystems)
+#
+##   This setup will generate a 'full' image which supports most systems.
+##   No autodetection is done.
+#    HOOKS=(base udev block filesystems)
+#
+##   This setup assembles a pata mdadm array with an encrypted root FS.
+##   Note: See 'mkinitcpio -H mdadm' for more information on raid devices.
+#    HOOKS=(base udev block mdadm encrypt filesystems)
+#
+##   This setup loads an lvm2 volume group on a usb device.
+#    HOOKS=(base udev block lvm2 filesystems)
+#
+##   NOTE: If you have /usr on a separate partition, you MUST include the
+#    usr, fsck and shutdown hooks.
+
+HOOKS=(base udev autodetect modconf block encrypt lvm2 filesystems keyboard fsck)
+
+# COMPRESSION
+# Use this to compress the initramfs image. By default, zstd compression
+# is used. Use 'cat' to create an uncompressed image.
+#COMPRESSION="zstd"
+#COMPRESSION="gzip"
+#COMPRESSION="bzip2"
+#COMPRESSION="lzma"
+#COMPRESSION="xz"
+#COMPRESSION="lzop"
+#COMPRESSION="lz4"
+
+# COMPRESSION_OPTIONS
+# Additional options for the compressor
+#COMPRESSION_OPTIONS=()
+EOF
+mkinitcpio -p linux
+mkinitcpio -p linux-lts
+
+# sudoers
+
+cat <<EOF > /etc/sudoers
+## sudoers file.
+##
+## This file MUST be edited with the 'visudo' command as root.
+## Failure to use 'visudo' may result in syntax or file permission errors
+## that prevent sudo from running.
+##
+## See the sudoers man page for the details on how to write a sudoers file.
+##
+
+##
+## Host alias specification
+##
+## Groups of machines. These may include host names (optionally with wildcards),
+## IP addresses, network numbers or netgroups.
+# Host_Alias	WEBSERVERS = www1, www2, www3
+
+##
+## User alias specification
+##
+## Groups of users.  These may consist of user names, uids, Unix groups,
+## or netgroups.
+# User_Alias	ADMINS = millert, dowdy, mikef
+
+##
+## Cmnd alias specification
+##
+## Groups of commands.  Often used to group related commands together.
+# Cmnd_Alias	PROCESSES = /usr/bin/nice, /bin/kill, /usr/bin/renice, \
+# 			    /usr/bin/pkill, /usr/bin/top
+# Cmnd_Alias	REBOOT = /sbin/halt, /sbin/reboot, /sbin/poweroff
+
+##
+## Defaults specification
+##
+## You may wish to keep some of the following environment variables
+## when running commands via sudo.
+##
+## Locale settings
+# Defaults env_keep += "LANG LANGUAGE LINGUAS LC_* _XKB_CHARSET"
+##
+## Run X applications through sudo; HOME is used to find the
+## .Xauthority file.  Note that other programs use HOME to find   
+## configuration files and this may lead to privilege escalation!
+# Defaults env_keep += "HOME"
+##
+## X11 resource path settings
+# Defaults env_keep += "XAPPLRESDIR XFILESEARCHPATH XUSERFILESEARCHPATH"
+##
+## Desktop path settings
+# Defaults env_keep += "QTDIR KDEDIR"
+##
+## Allow sudo-run commands to inherit the callers' ConsoleKit session
+# Defaults env_keep += "XDG_SESSION_COOKIE"
+##
+## Uncomment to enable special input methods.  Care should be taken as
+## this may allow users to subvert the command being run via sudo.
+# Defaults env_keep += "XMODIFIERS GTK_IM_MODULE QT_IM_MODULE QT_IM_SWITCHER"
+##
+## Uncomment to use a hard-coded PATH instead of the user's to find commands
+# Defaults secure_path="/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin"
+##
+## Uncomment to send mail if the user does not enter the correct password.
+# Defaults mail_badpass
+##
+## Uncomment to enable logging of a command's output, except for
+## sudoreplay and reboot.  Use sudoreplay to play back logged sessions.
+# Defaults log_output
+# Defaults!/usr/bin/sudoreplay !log_output
+# Defaults!/usr/local/bin/sudoreplay !log_output
+# Defaults!REBOOT !log_output
+
+##
+## Runas alias specification
+##
+
+##
+## User privilege specification
+##
+root ALL=(ALL) ALL
+
+## Uncomment to allow members of group wheel to execute any command
+%wheel ALL=(ALL) ALL
+
+## Same thing without a password
+# %wheel ALL=(ALL) NOPASSWD: ALL
+
+## Uncomment to allow members of group sudo to execute any command
+# %sudo	ALL=(ALL) ALL
+
+## Uncomment to allow any user to run sudo if they know the password
+## of the user they are running the command as (root by default).
+# Defaults targetpw  # Ask for the password of the target user
+# ALL ALL=(ALL) ALL  # WARNING: only use this together with 'Defaults targetpw'
+
+## Read drop-in files from /etc/sudoers.d
+@includedir /etc/sudoers.d
+EOF
+
+cat <<EOF > /etc/default/grub
+# GRUB boot loader configuration
+
+GRUB_DEFAULT=0
+GRUB_TIMEOUT=5
+GRUB_DISTRIBUTOR="Arch"
+GRUB_CMDLINE_LINUX_DEFAULT="loglevel=3 cryptdevice=$ARCH_DEVICE2:$ARCH_VGNAME:allow-discards quiet"
+GRUB_CMDLINE_LINUX=""
+
+# Preload both GPT and MBR modules so that they are not missed
+GRUB_PRELOAD_MODULES="part_gpt part_msdos"
+
+# Uncomment to enable booting from LUKS encrypted devices
+GRUB_ENABLE_CRYPTODISK=y
+
+# Set to 'countdown' or 'hidden' to change timeout behavior,
+# press ESC key to display menu.
+GRUB_TIMEOUT_STYLE=menu
+
+# Uncomment to use basic console
+GRUB_TERMINAL_INPUT=console
+
+# Uncomment to disable graphical terminal
+#GRUB_TERMINAL_OUTPUT=console
+
+# The resolution used on graphical terminal
+# note that you can use only modes which your graphic card supports via VBE
+# you can see them in real GRUB with the command `vbeinfo'
+GRUB_GFXMODE=auto
+
+# Uncomment to allow the kernel use the same resolution used by grub
+GRUB_GFXPAYLOAD_LINUX=keep
+
+# Uncomment if you want GRUB to pass to the Linux kernel the old parameter
+# format "root=/dev/xxx" instead of "root=/dev/disk/by-uuid/xxx"
+#GRUB_DISABLE_LINUX_UUID=true
+
+# Uncomment to disable generation of recovery mode menu entries
+GRUB_DISABLE_RECOVERY=true
+
+# Uncomment and set to the desired menu colors.  Used by normal and wallpaper
+# modes only.  Entries specified as foreground/background.
+#GRUB_COLOR_NORMAL="light-blue/black"
+#GRUB_COLOR_HIGHLIGHT="light-cyan/blue"
+
+# Uncomment one of them for the gfx desired, a image background or a gfxtheme
+#GRUB_BACKGROUND="/path/to/wallpaper"
+#GRUB_THEME="/path/to/gfxtheme"
+
+# Uncomment to get a beep at GRUB start
+#GRUB_INIT_TUNE="480 440 1"
+
+# Uncomment to make GRUB remember the last selection. This requires
+# setting 'GRUB_DEFAULT=saved' above.
+#GRUB_SAVEDEFAULT=true
+
+# Uncomment to disable submenus in boot menu
+#GRUB_DISABLE_SUBMENU=y
+EOF
+
+grub-install --target=x86_64-efi --efi-directory=/boot --bootloader-id=grub_uefi --recheck
+
+cp /usr/share/locale/en\@quot/LC_MESSAGES/grub.mo /boot/grub/locale/en.mo
+grub-mkconfig -o /boot/grub/grub.cfg
+
+fallocate -l $ARCH_SWAPSIZE /swapfile
+chmod 600 /swapfile
+mkswap /swapfile
+
+cp /etc/fstab /etc/fstab.bak
+echo '/swapfile none swap sw 0 0' | tee -a /etc/fstab
